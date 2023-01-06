a91ff5ecbbd0bb4fbf27024dcc7242e3fa2189ef
funderberker <meellone01@gmail.com>
2023-01-06 21:49:57 +0800

    [savings account] rework savings account logic. handle distribution of profits

---
+++ src/C.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.17;
+
+library C {
+
+    uint256 internal constant RATIO_BASE = 1e18;
+    uint256 internal constant OWNERSHIP_BASE = 1e18;
+
+    address internal constant UNI_V3_POOL_USDC_ETH = address(0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640);
+}
--- src/SavingsAccount.sol
+++ src/SavingsAccount.sol
@@ -2,6 +2,7 @@
 
 pragma solidity 0.8.17;
 
+import "src/C.sol";
 import "src/Storage.sol";
 import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
@@ -12,16 +13,17 @@ import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 struct Crate {
     address supplier;
     address asset;
-    uint256 amount;
-    uint32 allowedInstructionSet;
+    // uint256 amount;
+    uint256 ownership; // arbitrary number representing % share of SSI supply.
+    uint32 instructions;
 }
-// uint32[] allowedInstructionSets;
+// uint32[] Instructionss;
 
 /**
  * The Savings Account is a pool of all allowed assets where suppliers can add or remove supply and
  * assign that supply to certain Instruction Sets. The Savings account is the layer between suppliers
  * and the Instruction sets.
- * 
+ *
  * - A Crate of supply can only be assigned to 1 Instruction Set.
  * - A user can provide multiple Crates with different configurations.
  */
@@ -30,23 +32,14 @@ contract SavingsAccount {
     // All supplier+asset crates. Will grow O(N).
     mapping(address => mapping(address => Crate)) crates;
     // Total amount of asset supplied to an Instruction set.
-    // mapping(address => mapping(uint32 => uint256)) suppliedAmounts;
-    mapping(uint32 => uint256) suppliedAmounts;
+    mapping(uint32 => uint256) suppliedAmounts; // e.g. Realized value
+    // Total amount of ownership in an Instruction set.
+    mapping(uint32 => uint256) ownershipAmounts;
     // Total amount of asset borrowed by an Instruction set.
-    // mapping(address => mapping(uint32 => uint256)) borrowedAmounts;
     mapping(uint32 => uint256) borrowedAmounts;
 
     // Suppliers -> Savings Account.
     function addSupply(address asset, uint256 amount) public payable {
-        // Create or update Crate.
-        Crate memory crate = crates[msg.sender][asset];
-        if (crate.supplier == address(0x0)) {
-            crate.supplier = msg.sender;
-            crate.asset = asset;
-            crate.amount = amount;
-        } else {
-            crate.amount = crate.amount + amount;
-        }
         // If using Eth.
         if (asset == address(0x0)) {
             require(msg.value == amount); // what happens if someone puts eth into the contract directly?
@@ -54,12 +47,12 @@ contract SavingsAccount {
         else {
             require(IERC20(asset).transfer(address(this), amount));
         }
-        _incrementSuppliedAmount(crate.allowedInstructionSet, amount);
+        RealizeProfits(crates[msg.sender][asset].instructions);
+        AddToOrInitCrate(msg.sender, asset, amount);
     }
 
-    function removeSupply(address asset, uint256 amount) public {
-        Crate memory crate = crates[msg.sender][asset];
-        crate.amount -= amount; // Protected by Sol 0.8 overflow reverts.
+    //Q Reentrancy vulnerabilities here?
+    function removeSupply(address asset, uint256 amount, bool realizeProfits) public {
         // If using Eth.
         if (asset == address(0x0)) {
             payable(msg.sender).transfer(amount);
@@ -67,82 +60,109 @@ contract SavingsAccount {
         else {
             require(IERC20(asset).transfer(address(this), amount));
         }
-        _decrementSuppliedAmount(crate.allowedInstructionSet, amount);
+        if (realizeProfits) {
+            RealizeProfits(crates[msg.sender][asset].instructions);
+        }
+        RemoveFromCrate(msg.sender, asset, amount);
     }
 
-    function setAssetAllowedInstructionSet(address asset, uint32 instructionSet) public {
-        Crate memory crate = crates[msg.sender][asset];
-        require(crate.supplier != address(0x0));
-        // What are the profit implications of migrating a suppliers position?
-        _decrementSuppliedAmount(crate.allowedInstructionSet, crate.amount);
-        _incrementSuppliedAmount(instructionSet, crate.amount);
+    function setCrateInstructions(address asset, uint32 instructions, bool realizeProfits) public {
+        if (realizeProfits) {
+            RealizeProfits(crates[msg.sender][asset].instructions);
+        }
+        uint256 amount = EmptyCrate(msg.sender, asset); //Q will it use less gas to not use a local variable here?
+        RealizeProfits(instructions);
+        AddToOrInitCrate(msg.sender, asset, amount);
+    }
+
+    function borrowSupply(uint32 instructions, uint256 amount) internal {
+        if (instructions == 0x0) {
+            return;
+        }
+        borrowedAmounts[instructions] += amount;
+        emit InstructionsBorrowIncreased(instructions, amount);
     }
 
     // Instruction Sets -> Savings Account.
-    function borrowSupply(uint32 instructionSet, uint256 amount) internal {
-        _decrementBorrowedAmount(instructionSet, amount);
-    }
-
-    function returnSupply(uint32 instructionSet, uint256 amount) internal {
-        _incrementBorrowedAmount(instructionSet, amount);
+    function returnSupply(uint32 instructions, uint256 amount) internal {
+        if (instructions == 0x0) {
+            return;
+        }
+        borrowedAmounts[instructions] -= amount;
+        emit InstructionsBorrowDecreased(instructions, amount);
     }
 
     // Public Helpers.
-    function getSuppliedAmount(uint32 instructionSet) public view returns (uint256) {
-        return suppliedAmounts[instructionSet];
+    function getSuppliedAmount(uint32 instructions) public view returns (uint256) {
+        return suppliedAmounts[instructions];
     }
 
-    function getBorrowedAmount(uint32 instructionSet) public view returns (uint256) {
-        return borrowedAmounts[instructionSet];
+    function getOwnershipAmount(uint32 instructions) public view returns (uint256) {
+        return ownershipAmounts[instructions];
     }
 
-    function getAvailableAmount(uint32 instructionSet) public view returns (uint256) {
-        return suppliedAmounts[instructionSet] - borrowedAmounts[instructionSet];
+    function getBorrowedAmount(uint32 instructions) public view returns (uint256) {
+        return borrowedAmounts[instructions];
     }
 
-    function getUtilizationRatio(uint32 instructionSet) public view returns (uint256) {
-        return borrowedAmounts[instructionSet] / suppliedAmounts[instructionSet];
+    function getAvailableAmount(uint32 instructions) public view returns (uint256) {
+        return suppliedAmounts[instructions] - borrowedAmounts[instructions];
+    }
+
+    function getUtilizationRatio(uint32 instructions) public view returns (uint256) {
+        return borrowedAmounts[instructions] / suppliedAmounts[instructions];
     }
 
     // Private Helpers.
 
-    // What are the profit implications when changing a suppliers position?
-    function _incrementSuppliedAmount(uint32 instructionSet, uint256 amount) private {
-        if (instructionSet == 0x0) {
-            return;
+    // Use asset==0x0 for Eth.
+    function AddToOrInitCrate(address supplier, address asset, uint256 amount) internal returns (Crate storage crate) {
+        crate = crates[supplier][asset];
+        if (crate.supplier == address(0x0)) {
+            crate.supplier = supplier;
+            crate.asset = asset;
+            // crate.instructions = 0; // Assigned to the null instructions.
         }
-        suppliedAmounts[instructionSet] += amount;
-        emit InstructionSetSupplyIncreased(instructionSet, amount);
+        uint256 ownership;
+        // If Instructions empty, init to arbitrary base value.
+        if (ownershipAmounts[crate.instructions] == 0) ownership = C.OWNERSHIP_BASE;
+        else ownership = amount / suppliedAmounts[crate.instructions] * ownershipAmounts[crate.instructions];
+        suppliedAmounts[crate.instructions] += amount;
+        ownershipAmounts[crate.instructions] += amount;
+        crate.ownership += ownership;
+        InstructionsSupplyIncreased(crate.instructions, amount);
     }
 
-    // What are the profit implications when changing a suppliers position?
-    function _decrementSuppliedAmount(uint32 instructionSet, uint256 amount) private {
-        if (instructionSet == 0x0) {
-            return;
-        }
-        suppliedAmounts[instructionSet] -= amount;
-        emit InstructionSetSupplyDecreased(instructionSet, amount);
+    // Use asset==0x0 for Eth.
+    function RemoveFromCrate(address supplier, address asset, uint256 amount) internal returns (Crate storage crate) {
+        crate = crates[supplier][asset];
+        uint256 ownership = amount / suppliedAmounts[crate.instructions] * ownershipAmounts[crate.instructions];
+        suppliedAmounts[crate.instructions] -= amount;
+        ownershipAmounts[crate.instructions] -= ownership;
+        crate.ownership -= ownership;
+        InstructionsSupplyDecreased(crate.instructions, amount);
     }
 
-    function _decrementBorrowedAmount(uint32 instructionSet, uint256 amount) private {
-        if (instructionSet == 0x0) {
-            return;
-        }
-        borrowedAmounts[instructionSet] -= amount;
-        emit InstructionSetBorrowDecreased(instructionSet, amount);
+    function EmptyCrate(address supplier, address asset) internal returns (uint256 amount) {
+        Crate storage crate = crates[supplier][asset]; //Q using storage is less guess, since it already is there?
+        amount = crate.ownership / ownershipAmounts[crate.instructions] * suppliedAmounts[crate.instructions];
+        RemoveFromCrate(supplier, asset, amount);
     }
 
-    function _incrementBorrowedAmount(uint32 instructionSet, uint256 amount) private {
-        if (instructionSet == 0x0) {
-            return;
-        }
-        borrowedAmounts[instructionSet] += amount;
-        emit InstructionSetBorrowIncreased(instructionSet, amount);
+    // Process all unrealized Lender profits for an SSI.
+    // Although the ownership system accounts for distribution of value after underlying changes
+    // in assets, it cannot account for unknown profit. Although some profit can be estimated 
+    // profit share cannot, thus this must  be called before entering/exiting or else
+    // unrealized profits will be unfairly distributed.
+    function RealizeProfits(uint32 instructions) private {
+        if (instructions == 0) {return;}
+        // Instructions i = new Instructions(instructions);
+        // i.TakeProfits();
     }
 
     // Events.
-    event InstructionSetSupplyIncreased(uint32 instructionSet, uint256 amount);
-    event InstructionSetSupplyDecreased(uint32 instructionSet, uint256 amount);
-    event InstructionSetBorrowIncreased(uint32 instructionSet, uint256 amount);
-    event InstructionSetBorrowDecreased(uint32 instructionSet, uint256 amount);
+    event InstructionsSupplyIncreased(uint32 instructions, uint256 amount);
+    event InstructionsSupplyDecreased(uint32 instructions, uint256 amount);
+    event InstructionsBorrowIncreased(uint32 instructions, uint256 amount);
+    event InstructionsBorrowDecreased(uint32 instructions, uint256 amount);
 }
