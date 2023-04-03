243966b2fa1412505f2f83fae7078e316454e820
funderberker <meellone01@gmail.com>
2023-04-03 15:31:28 +0800

    every module should impl a decode function, but cannot be defined at this level bc return type will be unique to each impl

--- src/modules/IModule.sol
+++ src/modules/IModule.sol
@@ -8,6 +8,7 @@ import {Agreement} from "src/bookkeeper/LibBookkeeper.sol";
 //      but punting on it for now.
 
 // interface IModule {
+// functions decodeParameters(bytes calldata parameters) external view returns (...);
 //     /// @notice verify compatibility between the agreement and the parameters.
 //     function verifyParameters(Agreement agreement) external view;
 // }
