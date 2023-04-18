91652b97804c3ce3bcddac6e26290b5d6a7e5a4b
funderberker <meellone01@gmail.com>
2023-04-18 17:46:38 +0900

    mock contracts for testing

---
+++ test/mocks/MockPosition.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.19;
+
+import {Position} from "src/terminal/IPosition.sol";
+
+contract MockPosition is Position {
+    uint256 currentAmount;
+
+    constructor(uint256 amount) {
+        currentAmount = amount;
+    }
+
+    function getAmount(bytes calldata) external view override returns (uint256) {
+        return currentAmount;
+    }
+
+    function _exit(bytes calldata) internal pure override returns (uint256) {
+        return 0;
+    }
+}
