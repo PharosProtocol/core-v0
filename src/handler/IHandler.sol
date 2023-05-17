71d92ecfdb637c56de68ab31ad86ab1d904325c6
funderberker <meellone01@gmail.com>
2023-05-17 14:52:51 +0800

    remove handlers

--- src/handler/IHandler.sol
+++ src/handler/IHandler.sol
@@ -1,15 +1,15 @@
-// SPDX-License-Identifier: UNLICENSED
+// // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity 0.8.19;
+// pragma solidity 0.8.19;
 
-import {Asset} from "src/LibUtil.sol";
+// import {Asset} from "src/LibUtil.sol";
 
-/*
- * Asset handler exists to standardize pushing assets in a Pharos compatible manner. Functions should all be called
- * as delegatecalls.
- */
+// /*
+//  * Asset handler exists to standardize pushing assets in a Pharos compatible manner. Functions should all be called
+//  * as delegatecalls.
+//  */
 
-interface IHandler {
-    function pushAssetToAccount(address accountAddr, bytes accountParameters, Asset asset, uint256 amount) external;
-    function pushAssetToAddress(address addr, Asset asset, uint256 amount) external;
-}
+// interface IHandler {
+//     // function pushAssetToAccount(address accountAddr, bytes accountParameters, Asset asset, uint256 amount) external;
+//     function pushAssetToAddress(address addr, Asset asset, uint256 amount) external;
+// }
--- src/handler/implementations.sol/ERC20Handler.sol
+++ src/handler/implementations.sol/ERC20Handler.sol
@@ -1,26 +1,26 @@
-// SPDX-License-Identifier: UNLICENSED
+// // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity 0.8.19;
+// pragma solidity 0.8.19;
 
-import {Asset} from "src/LibUtil.sol";
-import {IHandler} from "../IHandler.sol";
+// import {Asset} from "src/LibUtil.sol";
+// import {IHandler} from "../IHandler.sol";
 
-/*
- * Asset handler exists to standardize pushing assets in a Pharos compatible manner. Functions should all be called
- * as delegatecalls.
- */
+// /*
+//  * Asset handler exists to standardize pushing assets in a Pharos compatible manner. Functions should all be called
+//  * as delegatecalls.
+//  */
 
-contract ERC20Handler is IHandler {
-    function pushAssetToAccount(address accountAddr, bytes accountParameters, Asset asset, uint256 amount) external {
-        (bool lenderSuccess,) = accountAddr.delegatecall(
-            abi.encodeWithSignature(
-                "loadPush((bytes3,address,uint256,bytes),uint256,bytes)", asset, amount, accountParameters
-            )
-        );
-        require(lenderSuccess, "ERC20Handler loadPush failed");
-    }
+// contract ERC20Handler is IHandler {
+//     // function pushAssetToAccount(address accountAddr, bytes accountParameters, Asset asset, uint256 amount) external {
+//     //     (bool lenderSuccess,) = accountAddr.delegatecall(
+//     //         abi.encodeWithSignature(
+//     //             "loadPush((bytes3,address,uint256,bytes),uint256,bytes)", asset, amount, accountParameters
+//     //         )
+//     //     );
+//     //     require(lenderSuccess, "ERC20Handler loadPush failed");
+//     // }
 
-    function pushAssetToAddress(address addr, Asset asset, uint256 amount) external {
-        require(IERC20(asset.addr).transfer(to, amount), "ERC20Handler transfer failed");
-    }
-}
+//     function pushAssetToAddress(address addr, Asset asset, uint256 amount) external {
+//         require(IERC20(asset.addr).transfer(to, amount), "ERC20Handler transfer failed");
+//     }
+// }
