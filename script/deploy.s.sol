5207f0b9985ae7d1b8c02989a2a916274d20556c
funderberker <meellone01@gmail.com>
2023-06-12 16:52:29 +0800

    forge deploy script. gorli deployment details

---
+++ script/deploy.s.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.19;
+
+import "forge-std/Script.sol";
+import "src/bookkeeper/LibBookkeeper.sol";
+import "src/bookkeeper/Bookkeeper.sol";
+import "src/LibUtil.sol";
+import "src/modules/account/implementations/SoloAccount.sol";
+import "src/modules/oracle/implementations/StaticValue.sol";
+import "src/modules/assessor/implementations/StandardAssessor.sol";
+import "src/modules/liquidator/implementations/InstantCloseTakeCollateral.sol";
+import "src/modules/position/implementations/UniV3Hold.sol";
+
+// To install forge/cast:
+// https://book.getfoundry.sh/getting-started/installation
+// To create keystore, run:
+// cast wallet new [PATH_TO_A_DIRECTORY]
+// to deploy, run:
+// forge script script/deploy.s.sol:DeployScript --keystore $DEFAULT_KEYSTORE_PATH --sender $DEFAULT_KEYSTORE_ADDR --rpc-url $GOERLI_RPC_URL --broadcast -vv
+// if etherscan verification desired append:
+// --verify --etherscan-api-key $ETHERSCAN_TOKEN
+// Contract address can be seen in output json file.
+
+// NOTE do not forget to switch constant contract addresses to match deployment chain.
+
+contract DeployScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        // string memory seedPhrase = vm.readFile(".secret");
+        // uint256 privateKey = vm.deriveKey(seedPhrase, 0);
+        vm.startBroadcast();
+        // Utils utils = new Utils();
+        // LibBookkeeper libBookkeeper = new LibBookkeeper();
+        Bookkeeper bookkeeper = new Bookkeeper();
+        new SoloAccount(address(bookkeeper));
+        new StandardAssessor();
+        new InstantCloseTakeCollateral(address(bookkeeper));
+        new StaticUsdcPriceOracle();
+        new UniV3HoldFactory(address(bookkeeper));
+
+        vm.stopBroadcast();
+    }
+}
--- script/forge_goerli_deploy.sh
+++ script/forge_goerli_deploy.sh
@@ -1,10 +1,16 @@
 utilAddr=$(forge create src/LibUtil.sol:Utils --keystore $1 -c 5 -r $GOERLI_RPC_URL)
 utilAddr=$(echo $utilAddr |  egrep -o '0x[a-f0-9A-F]{40} ' | tail -n1)
 
+bookkeeperUtilsAddr=$(forge create --keystore $1 -c 5 -r $GOERLI_RPC_URL --libraries src/LibUtil.sol:Utils:$utilAddr src/bookkeeper/LibBookkeeper.sol:LibBookkeeper)
+bookkeeperUtilsAddr=$(echo $bookkeeperUtilsAddr |  egrep -o '0x[a-f0-9A-F]{40} ' | tail -n1)
 
-accountAddr=$(forge create --keystore $1 -c 5 -r $GOERLI_RPC_URL --libraries src/LibUtil.sol:Utils:$utilAddr src/modules/account/implementations/ERC20Account.sol:ERC20Account --constructor-args 0x0000000000000000000000000000000000000000)
+bookkeeperAddr=$(forge create --keystore $1 -c 5 -r $GOERLI_RPC_URL --libraries src/bookkeeper/LibBookkeeper.sol:LibBookkeeper:$bookkeeperUtilsAddr src/bookkeeper/Bookkeeper.sol:Bookkeeper)
+bookkeeperAddr=$(echo $bookkeeperAddr |  egrep -o '0x[a-f0-9A-F]{40} ' | tail -n1)
+
+accountAddr=$(forge create --keystore $1 -c 5 -r $GOERLI_RPC_URL --libraries src/LibUtil.sol:Utils:$utilAddr src/modules/account/implementations/SoloAccount.sol:SoloAccount --constructor-args $bookkeeperAddr)
 accountAddr=$(echo $accountAddr |  egrep -o '0x[a-f0-9A-F]{40} ' | tail -n1)
 
-echo $utilAddr
-echo $accountAddr
-
+echo "libUtilAddr: " $utilAddr
+echo "bookkeeperUtilsAddr: " $bookkeeperUtilsAddr
+echo "bookkeeperAddr: " $bookkeeperAddr
+echo "accountAddr: " $accountAddr
