7bd8f64480a8b319379a2e26499ddeb0338f7bd0
funderberker <meellone01@gmail.com>
2023-07-11 16:28:25 +0200

    public utils transfer

--- src/libraries/LibUtilsPublic.sol
+++ src/libraries/LibUtilsPublic.sol
@@ -10,21 +10,26 @@ import {IERC20} from "lib/openzeppelin-contracts//contracts/token/ERC20/IERC20.s
  */
 
 library LibUtilsPublic {
+    /// @notice send eth to address.
+    /// @dev vulnerable to reentrancy. ensure reentrancy safety in calling function.
+    function ethCallTransfer(address payable to, uint256 amount) public {
+        (bool success, ) = to.call{value: amount}("");
+        require(success, "UtilsPublic.ethCallTransfer failed");
+    }
+
     // SECURITY NOTE: did not account for fee on transfer ERC20s. Either need to update logic or restrict to non-fee ERC20s.
     //                Probably easiest to do the later and treat fee-based erc20s as a different asset type.
 
     /// @notice Transfers tokens from msg.sender to a recipient.
-    /// @dev Return value is optional.
-    function safeErc20Transfer(address token, address to, uint256 value) public {
-        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(IERC20.transfer.selector, to, value));
+    function safeErc20Transfer(address token, address to, uint256 amount) public {
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(IERC20.transfer.selector, to, amount));
         require(success && (data.length == 0 || abi.decode(data, (bool))), "UtilsPublic.safeErc20Transfer failed");
     }
 
     /// @notice Transfers tokens from the targeted address to the given destination.
-    /// @dev Return value is optional.
-    function safeErc20TransferFrom(address token, address from, address to, uint256 value) public {
+    function safeErc20TransferFrom(address token, address from, address to, uint256 amount) public {
         (bool success, bytes memory data) = token.call(
-            abi.encodeWithSelector(IERC20.transferFrom.selector, from, to, value)
+            abi.encodeWithSelector(IERC20.transferFrom.selector, from, to, amount)
         );
         require(success && (data.length == 0 || abi.decode(data, (bool))), "UtilsPublic.safeErc20TransferFrom failed");
     }
