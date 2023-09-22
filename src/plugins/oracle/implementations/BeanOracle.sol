df37642174bd80008e420ae9a1c8cdc70b060292
mod323 <46406371+mod323@users.noreply.github.com>
2023-09-22 09:46:03 -0600

    misc

--- src/plugins/oracle/implementations/BeanOracle.sol
+++ src/plugins/oracle/implementations/BeanOracle.sol
@@ -22,7 +22,7 @@ contract BeanOracle is Oracle {
     address public beanPriceContract = 0xBEA0e11282e2bB5893bEcE110cF199501e872bAd;
 
     function _value() internal view returns (uint256) {
-        //get ETH:BEAN price from BEANSTALK_PUMP = 0xBA510f10E3095B83a0F33aa9ad2544E22570a87C
+        //get ETH:BEAN price from BEANSTALK_PUMP = 0xBA510f10E3095B83a0F33aa9ad2544E22570a87C , BEAN:ETH_WELL = 0xBEA0e11282e2bB5893bEcE110cF199501e872bAd
         uint[] memory reserves = IInstantaneousPump(0xBA510f10E3095B83a0F33aa9ad2544E22570a87C).readInstantaneousReserves(0xBEA0e11282e2bB5893bEcE110cF199501e872bAd, C.BYTES_ZERO);
         uint256 ethBeanvalue;
         ethBeanvalue= ((reserves[0] * (1e30)) / reserves[1]); //price of ETH in Beans 18 dec precision
@@ -32,7 +32,7 @@ contract BeanOracle is Oracle {
         (, int256 answer, , ,) = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419).latestRoundData();
         uint256 ethUsdValue;
         ethUsdValue = uint256(answer) * C.RATIO_FACTOR / (10**uint256(8)); // adjusted for 18 dec precision
-        
+        // return USD:BEAN (number of Beans per USD) price with 18 dec precision 
         return ethBeanvalue*C.RATIO_FACTOR/ethUsdValue;
     }
 
